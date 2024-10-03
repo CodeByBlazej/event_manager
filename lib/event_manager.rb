c6929baa2482b9b470fda85151494088f02b2ee5
CodeByBlazej <codebyblazej@gmail.com>
2024-10-03 20:19:17 +0100

    shorten the method to oneliner

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -17,15 +17,7 @@ end
 
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    zipcode = '00000'
-  elsif zipcode.length < 5
-    zipcode = zipcode.rjust(5, '0')
-  elsif zipcode.length > 5
-    zipcode = zipcode[0..4]
-  else
-    zipcode
-  end
+  zipcode.to_s.rjust(5, '0')
 end
 
 contents = CSV.open(
